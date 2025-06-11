2181818c7eecad9e0a2772c8b4fd2215cb632f29
Tricky <trickya23@outlook.com>
2025-06-11 06:28:52 +0000

    fixing resend error

--- app/contact/actions.ts
+++ app/contact/actions.ts
@@ -1,3 +1,5 @@
+"use server";
+
 import { Resend } from "resend";
 import { ContactTemplate } from "@/components/email/contact-template";
 import { contactFormSchema, type ContactFormData } from "./contact";
@@ -5,8 +7,7 @@ import { contactFormSchema, type ContactFormData } from "./contact";
 // load environment variables
 require("dotenv").config();
 
-const resend = new Resend("re_fshYLYXi_LwN5WpFx5mXtoSFknQmuQsUQ");
-// const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend(process.env.RESEND_API_KEY!);
 
 export async function POST(data: ContactFormData) {
   const result = contactFormSchema.safeParse(data);
@@ -15,11 +16,10 @@ export async function POST(data: ContactFormData) {
     return { success: false, error: result.error.format() };
   }
 
-  const { firstName, lastName, email, phone, inquiryType, subject, message } =
-    result.data;
+  try {
+    const { firstName, lastName, email, phone, inquiryType, subject, message } = result.data;
 
-  const sendEmail = await resend.emails
-    .send({
+    const sendEmail = await resend.emails.send({
       from: "Yendaaky Jobs Center Website <info@yendaakyejobscenter.com>",
       to: ["Yendaaky <yendaakyejobcenter@proton.me>"],
       subject: `${inquiryType}: ${subject}`,
@@ -32,10 +32,11 @@ export async function POST(data: ContactFormData) {
         subject,
         message,
       }),
-    })
-    .then(
-      (data: any) => ({ success: true, data }),
-      (error: any) => ({ success: false, error }),
-    );
-  return sendEmail;
+    });
+
+    return { success: true, data: sendEmail };
+  } catch (error: any) {
+    console.error("Resend Error:", error.message); // Log the error
+    return { success: false, error: error.message };
+  }
 }
